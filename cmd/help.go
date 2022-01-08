178c4934efae3727fa1e70a6c4c8393d74b97950
H-Kiku482 <80496940+H-Kiku482@users.noreply.github.com>
2022-01-08 10:55:26 +0900

    Update help.go

--- cmd/help.go
+++ cmd/help.go
@@ -3,16 +3,16 @@ package cmd
 // help message
 
 func HelpText() string {
-	return `Usage:  printdbtables DATABASE
+	return `Usage:  printtables DATABASE
    or:  printtables -u root -p DATABASE -m FILE.md
    or:  printtables DATABASE -t FILE.txt
 
 Options:
     --help, -h          Print this message and exit.
     --user, -u          Select MySQL user. Default is root.
-    --password, -p      Use password for print tables.
-    --host, -h          Select this option if MySQL host is not '127.0.0.1'.
-    --port, -P          Sekect this option if MySQL port is not '3306'.
+    --password, -p      Use password for printing.
+    --host, -H          Select this option when the MySQL host is not 'localhost'.
+    --port, -P          Select this option when the MySQL port is not '3306'.
     --markdown, -m      Print as markdown file.
     --text, -t          Print as text file.
     --cat, -c           Display on terminal.`
